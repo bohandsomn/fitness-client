8849170e93d65eb099113711fc1e58623881b6cd
bohandsomn <bohdan.lukianchenko@gmail.com>
2023-07-03 20:49:03 +0300

    Add aliases

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'module:react-native-dotenv',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx'],
+        alias: {
+          '@/shared': './src/shared',
+          '@/entities': './src/entities',
+          '@/features': './src/features',
+          '@/widgets': './src/widgets',
+          '@/pages': './src/pages',
+          '@/processes': './src/processes',
+          '@/app': './src/app',
+        },
+      },
+    ],
   ],
 };
