9fa18039d0ab7408f9365d878fb21a4bcda055fa
bohandsomn <bohdan.lukianchenko@gmail.com>
2023-07-06 21:46:53 +0300

    Create conts

---
+++ src/shared/const/app.ts
@@ -0,0 +1,4 @@
+export enum AppConst {
+    APP_NAME = 'Fitness',
+    APP_SHORT_DESCRIPTION = 'Short description about the app'
+}
---
+++ src/shared/const/auth.ts
@@ -0,0 +1,4 @@
+export enum AuthConst {
+    LOG_IN = 'Log in',
+    CREATE_AN_ACCOUNT = 'Create an account',
+}
