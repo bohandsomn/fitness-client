67bd0ecae4d0ede93618425c3e97da8a80170b51
bohandsomn <bohdan.lukianchenko@gmail.com>
2023-07-07 13:12:20 +0300

    Add types for Navigation

--- src/shared/navigation/lib/useNavigation/useNavigation.ts
+++ src/shared/navigation/lib/useNavigation/useNavigation.ts
@@ -1,8 +1,9 @@
 import { useMemo } from 'react'
 import { useNavigation as useReactNativeNavigation } from '@react-navigation/native'
 import { Navigation, INavigation } from '../navigation'
+import { PageNames } from '../../const'
 
-export const useNavigation = <Route extends string = string>(): INavigation<Route> => {
+export const useNavigation = (): INavigation<PageNames> => {
     const reactNativeNavigation = useReactNativeNavigation()
     const navigation = useMemo(
         () => new Navigation(reactNativeNavigation),
