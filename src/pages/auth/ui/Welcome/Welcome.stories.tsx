19e59dbdeb10e0d21e5225dcea0e7748cd7c859a
bohandsomn <bohdan.lukianchenko@gmail.com>
2023-07-09 08:19:05 +0300

    Add WelcomeProvider to the Welcome page story

--- src/pages/auth/ui/Welcome/Welcome.stories.tsx
+++ src/pages/auth/ui/Welcome/Welcome.stories.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react'
 import { Welcome } from './Welcome'
+import { WelcomeProvider } from '@/entities'
 
 const WelcomeMeta = {
     title: 'Pages/Auth/Welcome',
     component: Welcome,
-    decorators: [(Story: FC) => <Story />],
+    decorators: [
+        (Story: FC) => (
+            <WelcomeProvider>
+                <Story />
+            </WelcomeProvider>
+        ),
+    ],
 }
 
 export default WelcomeMeta
